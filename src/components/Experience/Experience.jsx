53bd483a492c449a653d48f530bdc7113bc8b357
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-08-25 14:31:16 +0530

    added some experience details

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -42,6 +42,13 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
+                <h4>MATERIAL UI</h4>
+                <small className="text-light">Experienced</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill className="experience__details-icon" />
+              <div>
                 <h4>JAVASCRIPT</h4>
                 <small className="text-light">Experienced</small>
               </div>

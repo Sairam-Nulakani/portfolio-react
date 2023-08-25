1803fcf02e6c95f26b52e85ec5cf26d9952f07a0
Sairam Nulakani <sairamnulakani4@gmail.com>
2023-08-25 14:34:11 +0530

    added some more skills

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -65,7 +65,7 @@ const Experience = () => {
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
                 <h4>REACT JS</h4>
-                <small className="text-light">Intermediate</small>
+                <small className="text-light">Experienced</small>
               </div>
             </article>
             <article className="experience__details">
